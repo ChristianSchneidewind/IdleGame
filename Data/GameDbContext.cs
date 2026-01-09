6ab45c4861667d9e96844270612ec016bf85e43a
ChristianSchneidewind <gizmovienna@gmail.com>
2026-01-09 11:12:36 +0100

    feat: add 12 planets (#3)

--- Data/GameDbContext.cs
+++ Data/GameDbContext.cs
@@ -34,35 +34,24 @@ namespace GameIdle.Data
                 .HasIndex(x => new { x.PlayerGameStateId, x.PlanetId })
                 .IsUnique();
 
-            // ✅ Planeten-Seeding (jeder Planet muss gekauft werden!)
+            // Planeten-Seeding (jeder Planet muss gekauft werden!)
             modelBuilder.Entity<Planet>().HasData(
-                new Planet
-                {
-                    Id = 1,
-                    Name = "Earth",
-                    UnlockPriceCredits = 100,
-                    BaseUpgradeCost = 10,
-                    CostMultiplier = 1.35,
-                    BaseProductionPerSecond = 1
-                },
-                new Planet
-                {
-                    Id = 2,
-                    Name = "Moon",
-                    UnlockPriceCredits = 1_000,
-                    BaseUpgradeCost = 50,
-                    CostMultiplier = 1.40,
-                    BaseProductionPerSecond = 3
-                },
-                new Planet
-                {
-                    Id = 3,
-                    Name = "Mars",
-                    UnlockPriceCredits = 25_000,
-                    BaseUpgradeCost = 250,
-                    CostMultiplier = 1.45,
-                    BaseProductionPerSecond = 8
-                }
+                new Planet { Id = 1,  Name = "Earth",   UnlockPriceCredits = 100,        BaseUpgradeCost = 10,      CostMultiplier = 1.35, BaseProductionPerSecond = 1  },
+                new Planet { Id = 2,  Name = "Moon",    UnlockPriceCredits = 1_000,      BaseUpgradeCost = 50,      CostMultiplier = 1.40, BaseProductionPerSecond = 3  },
+                new Planet { Id = 3,  Name = "Mars",    UnlockPriceCredits = 25_000,     BaseUpgradeCost = 250,     CostMultiplier = 1.45, BaseProductionPerSecond = 8  },
+                new Planet { Id = 4,  Name = "Venus",   UnlockPriceCredits = 250_000,    BaseUpgradeCost = 1_200,   CostMultiplier = 1.42, BaseProductionPerSecond = 22 },
+
+                new Planet { Id = 5,  Name = "Mercury", UnlockPriceCredits = 2_500_000,  BaseUpgradeCost = 6_000,   CostMultiplier = 1.47, BaseProductionPerSecond = 70 },
+                new Planet { Id = 6,  Name = "Jupiter", UnlockPriceCredits = 25_000_000, BaseUpgradeCost = 30_000,  CostMultiplier = 1.50, BaseProductionPerSecond = 220 },
+                new Planet { Id = 7,  Name = "Saturn",  UnlockPriceCredits = 250_000_000,BaseUpgradeCost = 160_000, CostMultiplier = 1.52, BaseProductionPerSecond = 700 },
+                new Planet { Id = 8,  Name = "Uranus",  UnlockPriceCredits = 2_500_000_000, BaseUpgradeCost = 900_000, CostMultiplier = 1.54, BaseProductionPerSecond = 2_000 },
+
+                new Planet { Id = 9,  Name = "Neptune", UnlockPriceCredits = 25_000_000_000, BaseUpgradeCost = 5_000_000,  CostMultiplier = 1.55, BaseProductionPerSecond = 6_500 },
+                new Planet { Id = 10, Name = "Pluto",   UnlockPriceCredits = 250_000_000_000, BaseUpgradeCost = 28_000_000, CostMultiplier = 1.57, BaseProductionPerSecond = 22_000 },
+                new Planet { Id = 11, Name = "Ceres",   UnlockPriceCredits = 2_500_000_000_000, BaseUpgradeCost = 160_000_000, CostMultiplier = 1.58, BaseProductionPerSecond = 75_000 },
+                new Planet { Id = 12, Name = "Eris",    UnlockPriceCredits = 25_000_000_000_000, BaseUpgradeCost = 900_000_000, CostMultiplier = 1.60, BaseProductionPerSecond = 250_000 }
+
+
             );
         }
     }
---
+++ Migrations/20260109100101_AddMorePlanets.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GameIdle.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMorePlanets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Planets",
+                columns: new[] { "Id", "BaseProductionPerSecond", "BaseUpgradeCost", "CostMultiplier", "Name", "UnlockPriceCredits" },
+                values: new object[,]
+                {
+                    { 4, 22L, 1200L, 1.4199999999999999, "Venus", 250000L },
+                    { 5, 70L, 6000L, 1.47, "Mercury", 2500000L },
+                    { 6, 220L, 30000L, 1.5, "Jupiter", 25000000L },
+                    { 7, 700L, 160000L, 1.52, "Saturn", 250000000L },
+                    { 8, 2000L, 900000L, 1.54, "Uranus", 2500000000L },
+                    { 9, 6500L, 5000000L, 1.55, "Neptune", 25000000000L },
+                    { 10, 22000L, 28000000L, 1.5700000000000001, "Pluto", 250000000000L },
+                    { 11, 75000L, 160000000L, 1.5800000000000001, "Ceres", 2500000000000L },
+                    { 12, 250000L, 900000000L, 1.6000000000000001, "Eris", 25000000000000L }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Planets",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Planets",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Planets",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Planets",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Planets",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Planets",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Planets",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Planets",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Planets",
+                keyColumn: "Id",
+                keyValue: 12);
+        }
+    }
+}
